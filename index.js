2a5e81dac5c18a3f6a734987055e8a0d6fce8921
gialbanes <albanesgiovana@gmail.com>
2024-11-05 21:44:32 -0300

    v 0.10 Express Session, Middleware e Flash Messages

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ import connection from "./config/sequelize-config.js";
 import ClientesController from "./controllers/ClientesController.js";
 import ProdutosController from "./controllers/ProdutosController.js";
 import PedidosController from "./controllers/PedidosController.js";
+import UsersController from './controllers/UsersController.js';
+
+import session from "express-session";
+
+import Auth from "./middleware/Auth.js";
+
+import flash from "express-flash";
+app.use(flash())
+
+app.use(session({
+  secret: "lojasecret",
+  cookie: {maxAge: 3600000}, 
+  saveUninitialized: false,
+  resave: false
+}))
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -31,12 +46,15 @@ app.use(express.static("public"));
 app.use("/", ClientesController);
 app.use("/", ProdutosController);
 app.use("/", PedidosController);
+app.use("/", UsersController);
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", Auth, function (req, res) {
+  res.render("index", {
+    messages: req.flash()
+  });
 });
 
-const port = 8080;
+const port = 8081;
 app.listen(port, (error) => {
   if (error) {
     console.log(`Ocorreu um erro: ${error}`);
